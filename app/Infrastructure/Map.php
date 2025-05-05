a9169161b8b8d70ab30afb9cc2a89e2fddf72fa2
xoaltie <mezencevadaria96@yandex.ru>
2025-05-05 15:17:54 +0700

    feat: add Map skeleton

--- app/Infrastructure/Map.php
+++ app/Infrastructure/Map.php
@@ -2,4 +2,42 @@
 
 namespace App\Infrastructure;
 
-final class Map {}
+final class Map
+{
+    public const int WIDTH = 20;
+    public const int HEIGHT = 20;
+    private static ?Map $instance = null;
+
+    /**
+     * @propery array<mixed> $entityList
+     */
+    private array $entityList = [];
+
+    private function __construct() {}
+
+    public static function genInstance(): Map
+    {
+        if (self::$instance === null) {
+            self::$instance = new Map();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * @return array
+     */
+    public function getEntityList(): array
+    {
+        return $this->entityList;
+    }
+
+    /**
+     * @param array $entityList
+     * @return void
+     */
+    public function setEntityList(array $entityList): void
+    {
+        $this->entityList = $entityList;
+    }
+}
