0aa49704791b75d962f9787181c6eff525213663
xoaltie <mezencevadaria96@yandex.ru>
2025-05-10 13:02:26 +0700

    feat: add Coordinates serialization

---
+++ app/Coordinates.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+final class Coordinates
+{
+    public function __construct(
+        public int $row,
+        public int $column,
+    ) {}
+
+    public function convertToKey(): string
+    {
+        return "{$this->row},{$this->column}";
+    }
+
+    public function convertToObject(string $key): self
+    {
+        $coordinates = explode(",", $key);
+
+        return new Coordinates((int) $coordinates[0], (int) $coordinates[1]);
+    }
+}
