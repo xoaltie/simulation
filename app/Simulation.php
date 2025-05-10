98cd3e80615b8bee8cadc962741850c5ccd53b4b
xoaltie <mezencevadaria96@yandex.ru>
2025-05-10 13:10:35 +0700

    refactor: simplify Simulation start and initialization

--- app/Simulation.php
+++ app/Simulation.php
@@ -13,27 +13,30 @@ final class Simulation
     private Map $map;
     private Renderer $renderer;
 
-    /** @var array<int, ActionContract> $initActions  */
-    private array $initActions = [];
-    public function __construct()
-    {
-        $this->map = Map::genInstance();
-        $this->renderer = new Renderer();
-    }
-
     public function start(): void
     {
-        $this->prepareInitActions();
+        $this->map = new Map();
+        $this->renderer = new Renderer();
 
-        foreach ($this->initActions as $action) {
-            $action->make();
-        }
+        $this->init();
 
         $this->renderer->render($this->map);
     }
 
-    private function prepareInitActions(): void
+    public function init(): void
     {
-        $this->initActions[] = new MapPrepareAction($this->map);
+        $this->map->spawn(new Coordinates(5, 2), new Tree());
+        $this->map->spawn(new Coordinates(1, 8), new Tree());
+        $this->map->spawn(new Coordinates(0, 2), new Grass());
+        $this->map->spawn(new Coordinates(5, 7), new Grass());
+        $this->map->spawn(new Coordinates(8, 1), new Grass());
+        $this->map->spawn(new Coordinates(1, 5), new Predator());
+        $this->map->spawn(new Coordinates(5, 3), new Predator());
+        $this->map->spawn(new Coordinates(8, 8), new Herbivore());
+        $this->map->spawn(new Coordinates(2, 1), new Herbivore());
+        $this->map->spawn(new Coordinates(0, 9), new Herbivore());
+        $this->map->spawn(new Coordinates(9, 2), new Herbivore());
+        $this->map->spawn(new Coordinates(4, 4), new Rock());
+        $this->map->spawn(new Coordinates(8, 7), new Rock());
     }
 }
