c894655e0bdfe926211a4aa60236ff5497e77a18
xoaltie <mezencevadaria96@yandex.ru>
2025-05-10 13:10:58 +0700

    feat: add configuration file for Simulation

---
+++ app/SimulationSettings.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+final readonly class SimulationSettings
+{
+    public const int MAX_GRASS = 3;
+    public const int MAX_HERBIVORE = 4;
+    public const int MAX_PREDATOR = 2;
+}
